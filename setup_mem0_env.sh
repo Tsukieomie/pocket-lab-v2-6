4b1cb0b6358150b6628f7677fd1ebc498021fc34
Perplexity Computer <perplexity@computer>
2026-04-23 03:51:18 +0000

    add setup_mem0_env.sh for iSH device setup

---
+++ setup_mem0_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run this on iSH to set up mem0 key
+echo "MEM0_API_KEY=m0-4szOQ3KGHPLqKcNzVc060nPxplieu2oxZh8OAlil" > /root/.mem0_env
+chmod 600 /root/.mem0_env
+echo "mem0 env written to /root/.mem0_env"
